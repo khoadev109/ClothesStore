f31808fb8f1db357d27f1a8a322a1706f163ecff
Khoa <khoanguyen109@gmail.com>
2018-12-23 21:01:13 +0700

    Add approach description

--- ClothesStore/Program.cs
+++ ClothesStore/Program.cs
@@ -13,23 +13,45 @@ namespace ClothesStore
         ///  
         ///  Handle ordering from different suppliers
         ///  
-        ///  Extends to order by more rates: discount rate,... instead of buying rate and selling rate
+        ///  Extends app to be able to handle more rate types: wholesale rate, discount rate,...
         ///  - Purpose:
-        ///     + To prevent update concurrency quantity of clothes on specific record, but increase redundant data
-        ///     + Can add more rate type without breaking current system
+        ///     + Can store in stock more rate type of clothes without breaking current system
         ///  - Description:
-        ///     + Current is:
-        ///         * When buying clothes from supplier, add to stock with rate type is: Input
-        ///         * When selling clothes to customer, add to stock with rate type is: Output
-        ///     + When customer selling clothes:
-        ///         * The inventory will be: currentInputQuantity - (currentOutputQuantity + quantityInputOfCustomer) >= 0
-        ///     + When add new type of rate like: discount rate
-        ///         * Order will base on this rate calculate and subtract discount price out of order's total price for customer
-        ///         * The inventory will be: currentInputQuantity - (currentOutputQuantity + quantityInputOfCustomer + discountQuantity) >= 0
+        ///     + Current stock includes 2 rate's types:
+        ///         * When buying clothes from supplier, rate type is: Input
+        ///         * When selling clothes to customer, rate type is: Output
+        ///         * Base on these types can get approriate rate of clothes:
+        ///             => Selling rate: T-Shirt - $6 
+        ///             => Retail rate: T-Shirt - $12
+        ///     + When add more rate like wholesale rate:
+        ///         * Can get approriate rate of clothes:
+        ///             => Selling rate: T-Shirt - $6 
+        ///             => Retail rate: T-Shirt - $12
+        ///             => Wholesale rate: T-shirt - $10
         ///         
-        ///  Extends to order by more quantities: no charge quantity,... instead of buying quantity and selling quantity
+        ///  Extends app to be able to handle more quantity types: non-charge quantity,...
         ///  - Purpose:
-        ///     + Can add more quantity type without breaking current system
+        ///     + Can store in stock more quantity type of clothes without breaking current system
+        ///     + To prevent update quantity of clothes in stock on specific record
+        ///  - Description:
+        ///     + Current stock includes 2 quantity's types:
+        ///         * Input quantity type: quantity of buying clothes from supplier
+        ///         * Output quantity type: quantity of selling clothes to customer
+        ///         * When selling clothes to customer, the calculation of checking existing clothes in stock will be:
+        ///             => currentInputQuantity - (currentOutputQuantity + quantityInputOfCustomer) >= 0
+        ///     + When add more quantity like discount quantity:
+        ///         * The calculation will be: 
+        ///             => currentInputQuantity - (currentOutputQuantity + quantityInputOfCustomer + discountQuantity) >= 0
+        ///             
+        ///   For quantity type, current is store all stock's data in one object and distinguish data by quantity type. 
+        ///   So another approach is separate stock's data into single object:
+        ///     + A single object like StockInput: store clothes's data in stock have input quantity type
+        ///     + A single object like StockOutput: store clothes's data in stock have output quantity type
+        ///     + A single object like StockDiscount: store clothes's data in stock have discount quantity type
+        ///   Purpose:
+        ///    - Prevent large data issue when storing data in one object
+        ///    - Can get data from separate objects and calculate quantity more faster
+        ///   
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
